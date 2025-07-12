dbc9e5b5011a9a521a548ae5a18cccfea08c8497
oluwatobicode <odetokuntreasure6@gmail.com>
2025-07-12 01:51:03 +0100

    fixed

--- src/services/supabaseClient.ts
+++ src/services/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://txqdbjsnojxjypjcxfzy.supabase.com";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4cWRianNub2p4anlwamN4Znp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5NDYwMDYsImV4cCI6MjA2NjUyMjAwNn0VFYmIhBIw755Suor1DiecChWgbqrFups6Yd0WFw6m3w";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);

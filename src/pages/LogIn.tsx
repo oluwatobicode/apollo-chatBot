d342583ff7cd3d108cabac475ae895207a18ae0f
oluwatobicode <odetokuntreasure6@gmail.com>
2025-07-12 13:02:50 +0100

    fixed-some-werid-issues

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,19 +1,6 @@
-import { useNavigate } from "react-router-dom";
 import Login from "../components/Login/LogInForm";
-import { useSelector } from "react-redux";
-import { RootState } from "../app/store";
-import { useEffect } from "react";
 
 function LogIn() {
-  const { isAuthenticated } = useSelector((state: RootState) => state.auth);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      navigate("/chat");
-    }
-  }, [isAuthenticated, navigate]);
-
   return (
     <div>
       <Login />
